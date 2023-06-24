77a62e114ace4c7edece5f25bce9a51aa7928a3b
Admin <Admin@DESKTOP-DDAUC42>
2023-06-24 16:19:09 +0700

    Add project files.

---
+++ ZangTFT/KeyMouseHelper.cs
@@ -0,0 +1,140 @@
+﻿using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Point = System.Drawing.Point;
+using PointF = System.Drawing.PointF;
+
+namespace ZangTFT
+{
+    internal class KeyMouseHelper
+    {
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator System.Drawing.Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
+
+
+
+
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        public static Point GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+            // NOTE: If you need error handling
+            // bool success = GetCursorPos(out lpPoint);
+            // if (!success)
+
+            return lpPoint;
+        }
+
+
+        internal struct INPUT
+        {
+            public UInt32 Type;
+            public MOUSEKEYBDHARDWAREINPUT Data;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT Mouse;
+        }
+
+        internal struct MOUSEINPUT
+        {
+            public Int32 X;
+            public Int32 Y;
+            public UInt32 MouseData;
+            public UInt32 Flags;
+            public UInt32 Time;
+            public IntPtr ExtraInfo;
+        }
+
+        public static void ClickOnPoint(Point position)
+        {
+
+            /// set cursor on coords, and press mouse
+            Cursor.Position = new Point(position.X, position.Y);
+
+            var inputMouseDown = new INPUT();
+            inputMouseDown.Type = 0; /// input type mouse
+            inputMouseDown.Data.Mouse.Flags = 0x0002; /// left button down
+
+            var inputMouseUp = new INPUT();
+            inputMouseUp.Type = 0; /// input type mouse
+            inputMouseUp.Data.Mouse.Flags = 0x0004; /// left button up
+
+            var inputs = new INPUT[] { inputMouseDown, inputMouseUp };
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+
+        public static void DoMouseClick(Point pt)
+        {
+            var X = Cursor.Position.X;
+            var Y = Cursor.Position.Y;
+
+            //move to coordinates
+            Cursor.Position = pt;
+
+            //perform click            
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+
+        public static void LinearSmoothMove(Point newPosition, int steps)
+        {
+            Point start = GetCursorPosition();
+            PointF iterPoint = start;
+
+            // Find the slope of the line segment defined by start and newPosition
+            PointF slope = new PointF(newPosition.X - start.X, newPosition.Y - start.Y);
+
+            // Divide by the number of steps
+            slope.X = slope.X / steps;
+            slope.Y = slope.Y / steps;
+
+            // Move the mouse to each iterative point.
+            for (int i = 0; i < steps; i++)
+            {
+                iterPoint = new PointF(iterPoint.X + slope.X, iterPoint.Y + slope.Y);
+                Cursor.Position = Point.Round(iterPoint);
+                Thread.Sleep(3);
+            }
+
+            // Move the mouse to the final destination.
+            Cursor.Position = newPosition;
+        }
+    }
+}
---
+++ ZangTFT/Main.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Drawing;
+
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
+using System.Xml.Linq;
+using IronOcr;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Threading;
+using BitMiracle.LibTiff.Classic;
+using System.IO;
+
+namespace ZangTFT
+{
+    public partial class Main : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
+        IntPtr LOLWindowHandle;
+        
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            LOLWindowHandle = GetLOLWindow();
+            if (LOLWindowHandle == IntPtr.Zero)
+            {
+                //MessageBox.Show("Cannot find LOL Windows, exit now");
+                //Application.Exit();
+            }
+        }
+
+        public bool LOLIsActivated()
+        {
+            var activatedHandle = GetForegroundWindow();
+            if (activatedHandle == IntPtr.Zero)
+            {
+                return false;       // No window is currently activated
+            }
+
+            return activatedHandle == LOLWindowHandle;
+        }
+
+        public void Test()
+        {
+            string imageName = "Screenshot\\Giang.png";
+            int w = 1015;
+            int h = 35;
+            int left = 472;
+            int top = 1042;
+            var bmp = new Bitmap(w, h, PixelFormat.Format32bppArgb);
+
+            imageName = "Screenshot\\Giang.png";
+            /*
+            using (Graphics graphics = Graphics.FromImage(bmp))
+            {
+                graphics.CopyFromScreen(left, top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            }
+
+            bmp.Save(imageName, ImageFormat.Png);
+            */
+
+            for (int i = 0; i < 5; i++)
+            {
+                // Create variable for MatchTemplate function
+                Mat templ = CvInvoke.Imread("Jhin.png", ImreadModes.Grayscale);
+                Mat img = CvInvoke.Imread(imageName, ImreadModes.Grayscale);
+                Mat result = new Mat();
+
+                // Find all position matching with template
+
+
+                // MatchTemplate function
+                CvInvoke.MatchTemplate(img, templ, result, TemplateMatchingType.CcoeffNormed);
+
+
+
+                // Normalize the result image into a matrix of floats (thresholds?)
+                double threshold = 0.8;
+                Mat thresholds = new Mat();
+                CvInvoke.Normalize(result, thresholds, 0, 1, Emgu.CV.CvEnum.NormType.MinMax);
+
+                var rectangles = new List<Rectangle>();
+                var size = new Size(templ.Width, templ.Height);
+
+                // Convert it to a multidimensional array to be able to iterate through it
+                // (is it really necessary, isn't something native in EmguCV for this?)
+                var thresholdData = thresholds.GetData();
+                for (int y = 0; y < thresholdData.GetLength(0); y++)
+                {
+                    for (int x = 0; x < thresholdData.GetLength(1); x++)
+                    {
+                        var value = (float)thresholdData.GetValue(y, x);
+
+                        if (value > threshold)
+                        {
+                            rectangles.Add(new Rectangle(x, y, size.Width, size.Height));
+                        }
+                    }
+                }
+
+                // Draw rectangles around the matching positions
+                foreach (var rectangle in rectangles)
+                {
+                    CvInvoke.Rectangle(img, rectangle, new MCvScalar(0, 0, 255), 2);
+                }
+
+                // Save the image
+                img.Save("Result\\LOL_Screenshot" + i + ".png");
+
+            }
+        }
+
+
+        public void AutoClick()
+        {
+            // List of champions
+            List<string> champions = new List<string>();
+            /*
+            champions.Add("Kalista");
+            champions.Add("Irelia");
+            champions.Add("Maokai");
+            champions.Add("Samira");
+            champions.Add("Warwick");
+            champions.Add("Kaisa");
+            champions.Add("Shen");
+            champions.Add("Yasuo");
+            champions.Add("Aatrox");
+            */
+            champions.Add("Poppy");
+            champions.Add("Peppy");
+            champions.Add("Tristana");
+            champions.Add("Mackai");
+            champions.Add("Maokai");
+            champions.Add("Viego");
+            champions.Add("Vieeo");
+            champions.Add("Jinx");
+            champions.Add("Jayce");
+            champions.Add("Zeri");
+            champions.Add("Heimerdinger");
+
+            Random rnd = new Random();
+            string imageName = "Screenshot\\LOL_Screenshot"+rnd.Next()+".png";
+            IronTesseract ocr = new IronTesseract();
+            ocr.Configuration.BlackListCharacters = "~`$#^*_}{][|\\";
+            ocr.Configuration.PageSegmentationMode = TesseractPageSegmentationMode.SingleWord;
+
+            int w = 1015;
+            int h = 35;
+            int left = 472;
+            int top = 1042;
+            var bmp = new Bitmap(w, h, PixelFormat.Format32bppArgb);
+
+            do
+            {
+                imageName = "Screenshot\\LOL_Screenshot" + rnd.Next() + ".png";
+                using (Graphics graphics = Graphics.FromImage(bmp))
+                {
+                    graphics.CopyFromScreen(left, top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+                }
+
+                bmp.Save(imageName, ImageFormat.Png);
+
+                for (int i = 0; i < 5; i++)
+                {
+                    using (OcrInput input = new OcrInput())
+                    {
+                        // Dimensions are in pixel
+                        var contentArea = new System.Drawing.Rectangle() { X = w / 5 * i, Y = 0, Height = h, Width = w / 5 };
+                        input.Add(imageName, contentArea);
+                        OcrResult result = ocr.Read(input);
+                        Console.WriteLine(result.Text);
+
+                        // If text in result contains any champion name, click on that champion
+                        foreach (string champ in champions)
+                        {
+                            if (result.Text.Contains(champ))
+                            {
+                                int rdX = rnd.Next(20, w / 5 - 20);
+                                int rdY = rnd.Next(10, 40);
+                                KeyMouseHelper.ClickOnPoint(new Point(left + w / 5 * i + rdX, top - rdY));
+                                Thread.Sleep(50);
+                            }
+                        }
+                    }
+                }
+                Thread.Sleep(1);
+            } while (true);
+        }
+
+        public void GenerateChamps()
+        {
+            // Read all image in folder named "Screenshot"
+            string[] files = Directory.GetFiles("Screenshot");
+            foreach (string file in files)
+            {
+                // Crop image into 5 parts then crop first 50px
+                Bitmap bmp = new Bitmap(file);
+                int w = bmp.Width / 5;
+                int h = bmp.Height;
+                int left = 0;
+                int top = 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Bitmap crop = bmp.Clone(new Rectangle(left + 9, top + 3, 80, 17), bmp.PixelFormat);
+                    // Save crop to temp file
+                    crop.Save("temp.png", ImageFormat.Png);
+
+                    // Read text on image
+                    IronTesseract ocr = new IronTesseract();
+                    ocr.Configuration.BlackListCharacters = "~`$#^*_}{][|\\";
+                    ocr.Configuration.PageSegmentationMode = TesseractPageSegmentationMode.SingleWord;
+                    OcrResult result = ocr.Read("temp.png");
+                    Console.WriteLine(result.Text);
+
+                    // Save image
+                    crop.Save("Champs\\" + RemoveSpecialCharacters(result.Text) + ".png", ImageFormat.Png);
+                    left += w;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // KeyMouseHelper.ClickOnPoint(LOLWindowHandle, new Point(900, 670));
+            //string Text = new IronTesseract().Read(@"FFF.png").Text;
+            //MessageBox.Show(Text);
+            
+            // Create new thread run AutoClick
+            Thread thread = new Thread(AutoClick);
+            thread.Start();
+        }
+
+        private IntPtr GetLOLWindow()
+        {
+            IntPtr hWnd = IntPtr.Zero;
+            foreach (Process pList in Process.GetProcesses())
+            {
+                if (pList.MainWindowTitle.Contains("League of Legends"))
+                {
+                    hWnd = pList.MainWindowHandle;
+                }
+            }
+            return hWnd;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Test();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GenerateChamps();
+        }
+
+        public string RemoveSpecialCharacters(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
